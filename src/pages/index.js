233ed11b70480d9bd3f998c8f0a972e839bab574
Horvath Attila <59293104+sweeptic@users.noreply.github.com>
2021-03-10 10:54:29 +0100

    30. survey setup

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import { graphql } from 'gatsby';
-import { Layout, Hero, About, Projects, Survey, Slider, GridProjects } from '../components';
+import {
+  Layout,
+  Hero,
+  About,
+  Projects,
+  Survey,
+  Slider,
+  GridProjects,
+} from '../components';
 import SEO from '../components/seo';
 
 const HomePage = ({ data }) => {
@@ -14,13 +22,18 @@ const HomePage = ({ data }) => {
       <About />
       <Projects projects={projects} title='latest projects' />
       <Slider />
+      <Survey />
     </Layout>
   );
 };
 
 export const query = graphql`
   {
-    allAirtable(filter: { table: { eq: "Projects" } }, limit: 3, sort: { fields: data___date, order: DESC }) {
+    allAirtable(
+      filter: { table: { eq: "Projects" } }
+      limit: 3
+      sort: { fields: data___date, order: DESC }
+    ) {
       nodes {
         id
         data {
